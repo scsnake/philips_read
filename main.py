d1f694d695402acc89404d70a5199f9ef5fcb30e
scsnake <scsnake@gmail.com>
2018-06-12 15:28:02 +0800

    TMP

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from glob import glob
 from collections import OrderedDict
 import numpy as np
 
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
 def get_centerlines(series_dir):
     ret = OrderedDict()
 
@@ -27,11 +31,36 @@ def get_centerlines(series_dir):
 
 
 if __name__ == '__main__':
-    centerlines = get_centerlines(r'D:\Users\ntuhuser\Downloads\Cad\STATE0 - 805')
-    a = centerlines['LAD']
-    l = []
+    np.set_printoptions(formatter={'float_kind':  lambda x: "%.2f" % x})
 
-    for i in range(a.shape[1]):
-        l+= list(a[i, 0:3])
+    # centerlines = get_centerlines(r'C:\Users\Administrator\Downloads\Cad\STATE0 - 805')
+    centerlines = get_centerlines(r'C:\Users\Administrator\Downloads\2994481\STATE0 - 3015')
 
-    print(l)
+    print(centerlines['LAD'][0])
+
+    # data = []
+    # data2 = []
+    # for vessel, points in centerlines.items():
+    #     data += list(points[:, 0:3])
+    # for i in range(3):
+    #     data[i]=centerlines['LAD'][i, 0:3]
+    #
+    #
+    # data2+=[list(centerlines['LAD'][0, 0:3]+centerlines['LAD'][0, 3:6])]
+    # data2+=[list(centerlines['LAD'][1, 0:3]+centerlines['LAD'][1, 3:6])]
+    # data2+=[list(centerlines['LAD'][1, 0:3]+centerlines['LAD'][1, 6:9])]
+
+    # data = np.array(data)
+    # data2 = np.array(data2)
+
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    for vessel, points in centerlines.items():
+        data = points[:, 0:3]
+        ax.scatter(data[:, 0], data[:, 1], data[:, 2], c=np.random.rand(3,))
+    # ax.scatter(data2[:, 0], data2[:, 1], data2[:, 2], color='red')
+    for angle in range(0, 360, 10):
+        ax.view_init(30, angle)
+        # plt.draw()
+        plt.savefig('coronary%d.png' % (angle, ))
+        # plt.pause(.001)
