0801ca35ad86e295a6eb0233e1d5be518bc248cb
scsnake <scsnake@gmail.com>
2018-06-12 18:39:39 +0800

    implement oblique mpr

--- main.py
+++ main.py
@@ -1,11 +1,64 @@
-import pydicom
-from pathlib import Path
-from glob import glob
+import random
 from collections import OrderedDict
-import numpy as np
+from glob import glob
+from pathlib import Path
 
 import matplotlib.pyplot as plt
+import numpy as np
+import pydicom
 from mpl_toolkits.mplot3d import Axes3D
+from scipy.ndimage import map_coordinates
+
+
+def get_float_index(arr, index):
+    shape = np.array(arr.shape)
+    index = np.array(index)
+    if not (np.all(index > np.zeros(index.shape)) and np.all(index < shape)):
+        return
+
+
+def two_vec_on_plane(normal_vector):
+    if normal_vector[2] == 0:
+        if normal_vector[1] == 0:
+            return np.array([0, 1, 0]), np.array([0, 0, 1])
+        elif normal_vector[0] == 0:
+            return np.array([1, 0, 0]), np.array([0, 0, 1])
+        else:
+            y = random.uniform(0, 1)
+            z = random.uniform(0, 1)
+
+            vec1 = np.array([(-1) * np.dot(normal_vector[1:3], np.array([y, z])) / normal_vector[0], y, z])
+            vec1 /= np.linalg.norm(vec1)
+
+            x = random.uniform(0, 1)
+            y = random.uniform(0, 1)
+            vec2 = np.array([(-1) * np.dot(normal_vector[1:3], np.array([y, z])) / normal_vector[0], y, z])
+            vec2 -= np.dot(vec1, vec2) * vec1
+            vec2 /= np.linalg.norm(vec2)
+    elif normal_vector[0] == 0 and normal_vector[1] == 0:
+        return np.array([1, 0, 0]), np.array([0, 1, 0])
+    else:
+        x = random.uniform(0, 1)
+        y = random.uniform(0, 1)
+
+        vec1 = np.array([x, y, (-1) * np.dot(normal_vector[0:2], np.array([x, y])) / normal_vector[2]])
+        vec1 /= np.linalg.norm(vec1)
+
+        x = random.uniform(0, 1)
+        y = random.uniform(0, 1)
+        vec2 = np.array([x, y, (-1) * np.dot(normal_vector[0:2], np.array([x, y])) / normal_vector[2]])
+        vec2 -= np.dot(vec1, vec2) * vec1
+        vec2 /= np.linalg.norm(vec2)
+    return vec1, vec2
+
+
+def oblique_MPR(volume, spacing, point, normal_vector_plane, width, output_shape):
+    u, v = two_vec_on_plane(normal_vector_plane)
+    coords = (u[:, None, None] * np.linspace(width / -2.0, width / 2.0, output_shape[0])[None, :, None] +
+              v[:, None, None] * np.linspace(width / -2.0, width / 2.0, output_shape[1])[None, None, :])
+
+    idx = coords / spacing[(slice(None),) + (None,) * (coords.ndim - 1)] + point[:, None, None]
+    new_data = map_coordinates(volume, idx, order=1, mode='constant', cval=0)
 
 
 def get_centerlines(series_dir):
@@ -31,7 +84,10 @@ def get_centerlines(series_dir):
 
 
 if __name__ == '__main__':
-    np.set_printoptions(formatter={'float_kind':  lambda x: "%.2f" % x})
+    np.set_printoptions(formatter={'float_kind': lambda x: "%.2f" % x})
+
+    data = np.random.rand(10, 10, 10)
+    print(oblique_MPR(data, np.array([1, 2, 1]), np.array([5, 5, 5]), np.array([1, 0, 0]), 3, (3, 3)))
 
     # centerlines = get_centerlines(r'C:\Users\Administrator\Downloads\Cad\STATE0 - 805')
     centerlines = get_centerlines(r'C:\Users\Administrator\Downloads\2994481\STATE0 - 3015')
@@ -57,10 +113,10 @@ if __name__ == '__main__':
     ax = Axes3D(fig)
     for vessel, points in centerlines.items():
         data = points[:, 0:3]
-        ax.scatter(data[:, 0], data[:, 1], data[:, 2], c=np.random.rand(3,))
+        ax.scatter(data[:, 0], data[:, 1], data[:, 2], c=np.random.rand(3, ))
     # ax.scatter(data2[:, 0], data2[:, 1], data2[:, 2], color='red')
     for angle in range(0, 360, 10):
         ax.view_init(30, angle)
         # plt.draw()
-        plt.savefig('coronary%d.png' % (angle, ))
+        plt.savefig('coronary%d.png' % (angle,))
         # plt.pause(.001)
